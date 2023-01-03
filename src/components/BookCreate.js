c85df26931f0565ce4b02ff3f3475ba4afe12501
lawyerh <lawyerh_01@yahoo.com>
2023-01-02 19:09:46 -0800

    css reset added as well as styling for Create Book component

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -16,13 +16,16 @@ export default function BookCreate({ onSubmitBookTitle }) {
     <section className="add-book">
       <h2 className="add-book__heading">Add a Book</h2>
       <form onSubmit={handleSubmit}>
-        <label className="add-book__input-label">Title</label>
-        <input
-          value={bookTitle}
-          onChange={handleChange}
-          className="add-book__input"
-          placeholder="Book title.."
-        />
+        {/* wrapped in div to send button down a line */}
+        <div>
+          <input
+            value={bookTitle}
+            onChange={handleChange}
+            className="add-book__input"
+            placeholder="Book Title.."
+          />
+          <p className="add-book__arrow">&larr;</p>
+        </div>
         <button onSubmit={handleSubmit} className="button add-book__submit">
           Submit
         </button>
